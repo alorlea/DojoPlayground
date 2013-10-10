24f26ce64d7c4ec5f46fa5589922d4ece0005d24
alorlea <a.lorenteleal@gmail.com>
2013-10-10 17:25:25 +0200

    added new lessons done

---
+++ Learn-Python-Hard-Way/problem37.py
@@ -0,0 +1,18 @@
+num=170
+
+print 'the number is in decimal %d' % num
+print 'the number is in decimal %i'% num
+print 'the number is in octal %o'% num
+print 'the number is in unsigned decimal %u'% num
+print 'the number is in hex %x'% num
+print 'the number is in hex uppercase %X'% num
+print 'the number is in scientific %e'% num
+print 'the number is in scientific uppercase %E'% num
+print 'the number is in float decimal format%f'% num
+print 'the number is in float decimal format uppercase %F'% num
+print 'the number is in float point with exp %g'% num
+print 'the number is in idem uppercase %G'% num
+print 'the number is in character %c'% num
+print 'the number is in raw %r'% num
+print 'the number is in string %s'% num
+print 'the number is no arg %%'% num
